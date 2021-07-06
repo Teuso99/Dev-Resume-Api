4fa97ec76c9cd876e1516be702cfc3111c510b84
Mateus Machado <mateusmmachado2@gmail.com>
2021-07-05 22:17:49 -0300

    First Commit

---
+++ DevResumeApi/User.cs
@@ -0,0 +1,17 @@
+using System;
+
+namespace DevResumeApi
+{
+    public class User
+    {
+        public Guid Id { get; set; }
+
+        public string FirstName { get; set; }
+
+        public string LastName { get; set; }
+
+        public string Email { get; set; }
+
+        public string Password { get; set; }
+    }
+}
---
+++ DevResumeApiTests/UserControllerTests.cs
@@ -0,0 +1,14 @@
+using System;
+using Xunit;
+
+namespace DevResumeApiTests
+{
+    public class UserControllerTests
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
