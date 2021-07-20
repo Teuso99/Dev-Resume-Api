09f76db4567c67b4824be53dc3914e92370b120d
Mateus Machado <mateusmmachado2@gmail.com>
2021-07-20 17:45:53 -0300

    Adiciona InMemoryDb e rotas async

--- DevResumeApi/Controllers/UserController.cs
+++ DevResumeApi/Controllers/UserController.cs
@@ -2,7 +2,9 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using DevResumeApi.Models;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Logging;
 
 namespace DevResumeApi.Controllers
@@ -12,17 +14,17 @@ namespace DevResumeApi.Controllers
     public class UserController : ControllerBase
     {
 
-        private readonly List<User> _users;
+        private readonly UserContext _context;
 
-        public UserController(List<User> users)
+        public UserController(UserContext context)
         {
-            _users = users;
+            _context = context;
         }
 
         [HttpGet]
-        public ActionResult<List<User>> GetAllUsers()
+        public async Task<ActionResult<List<User>>> GetAllUsers()
         {
-            return _users.ToList();
+            return await _context.Users.ToListAsync();
         }
 
         [HttpGet("{id}")]
@@ -33,7 +35,7 @@ namespace DevResumeApi.Controllers
                 return new NotFoundObjectResult("ID not provided!");
             }
 
-            var user = _users.SingleOrDefault(m => m.Id == id);
+            var user = _context.Users.SingleOrDefault(m => m.Id == id);
 
             if (user == null)
             {
@@ -44,7 +46,7 @@ namespace DevResumeApi.Controllers
         }
 
         [HttpPost]
-        public ActionResult PostUser(User user)
+        public async Task<ActionResult> PostUser(User user)
         {
             if (! ModelState.IsValid)
             {
@@ -52,49 +54,54 @@ namespace DevResumeApi.Controllers
             }
 
             user.Id = Guid.NewGuid();
-            _users.Add(user);
+            _context.Users.Add(user);
+            await _context.SaveChangesAsync();
 
-            return CreatedAtAction("Get", user);
+            return CreatedAtAction("GetAllUsers",  user);
         }
 
         [HttpDelete("{id}")]
-        public ActionResult DeleteUser(Guid id)
+        public async Task<ActionResult> DeleteUser(Guid id)
         {
-            var userToDelete = _users.SingleOrDefault(m => m.Id == id);
+            var userToDelete = await _context.Users.FindAsync(id);
 
             if (userToDelete == null)
             {
                 return new NotFoundObjectResult(userToDelete);
             }
 
-            _users.Remove(userToDelete);
+            _context.Users.Remove(userToDelete);
+            await _context.SaveChangesAsync();
 
             return new OkObjectResult("User " + userToDelete.Email + " deleted");
         }
 
         [HttpPut("{id}")]
-        public ActionResult PutUser(Guid? id, User user)
+        public async Task<ActionResult> PutUser(Guid? id, User user)
         {
             if (id == null || user == null)
             {
                 return new NotFoundObjectResult("User or UserId is missing!");
             }
 
-            var userToUpdate = _users.FirstOrDefault(m => m.Id == id);
-            _users.Remove(userToUpdate);
+            var userToUpdate = await _context.Users.FindAsync(id);
 
             if (userToUpdate == null)
             {
                 return new NotFoundObjectResult("User with UserId is missing!");
             }
 
+            _context.Users.Remove(userToUpdate);
+            await _context.SaveChangesAsync();
+            
             userToUpdate.Id = (Guid)id;
             userToUpdate.FirstName = user.FirstName;
             userToUpdate.LastName = user.LastName;
             userToUpdate.Email = user.Email;
             userToUpdate.Password = user.Password;
 
-            _users.Add(userToUpdate);
+            _context.Users.Add(userToUpdate);
+            await _context.SaveChangesAsync();
 
             return new OkObjectResult(userToUpdate);
         }
